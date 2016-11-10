41605e2b4b85e6bb9c5490b0870df519b7b83fbb
mrimes19 <mrimes19@LAB-LIB-223-02>
2016-11-10 12:53:31 -0800

    Initial commit

---
+++ exam2/exam2/Source.cpp
@@ -0,0 +1,17 @@
+//
+//Marnie Rimes
+//11-10-16
+//CS172 Exam2
+//
+//I affirm that all code given below was written solely by me, Marnie Rimes,
+//and that any help I received adhered to the rules stated for this exam
+//
+//look at grading rubric and fulfill the parts with a lot of points
+//and comment a lot
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+}
